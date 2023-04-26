c3196d52ba63b742e478c0fb95a33e804fb6da24
Arturo Suárez <manuel.suarez@cimat.mx>
2023-04-26 11:52:33 -0600

    Configure dataset directories

--- main.py
+++ main.py
@@ -16,4 +16,27 @@ if not os.path.exists(DATA_DIR):
     os.system('git clone https://github.com/alexgkendall/SegNet-Tutorial ./data')
     logging.info('Done!')
 
+x_train_dir = os.path.join(DATA_DIR, 'train')
+y_train_dir = os.path.join(DATA_DIR, 'trainannot')
+
+x_valid_dir = os.path.join(DATA_DIR, 'val')
+y_valid_dir = os.path.join(DATA_DIR, 'valannot')
+
+x_test_dir = os.path.join(DATA_DIR, 'test')
+y_test_dir = os.path.join(DATA_DIR, 'testannot')
+
+# helper function for data visualization
+def visualize(**images, figname):
+    """Plot images in one row."""
+    n = len(images)
+    plt.figure(figsize=(16, 5))
+    for i, (name, image) in enumerate(images.items()):
+        plt.subplot(1, n, i + 1)
+        plt.xticks([])
+        plt.yticks([])
+        plt.title(' '.join(name.split('_')).title())
+        plt.imshow(image)
+    plt.savefig(figname)
+    plt.close()
+
 logging.info('Done!')
