f01ef2dfba02e67fe0710bec72e9b154b347fec1
Arturo Suárez <manuel.suarez@cimat.mx>
2023-04-26 14:30:20 -0600

    Training

--- main.py
+++ main.py
@@ -178,4 +178,80 @@ for i in range(3):
     image, mask = augmented_dataset[1]
     visualize(f"figure{i+2}.png", image=image, mask=mask.squeeze(-1))
 
+# Model creation and training
+import torch
+import segmentation_models_pytorch as smp
+
+ENCODER = 'se_resnext50_32x4d'
+ENCODER_WEIGHTS = 'imagenet'
+CLASSES = ['car']
+ACTIVATION = 'sigmoid' # could be None for logits or 'softmax2d' for multiclass segmentation
+DEVICE = 'cuda'
+
+# create segmentation model with pretrained encoder
+model = smp.FPN(
+    encoder_name=ENCODER,
+    encoder_weights=ENCODER_WEIGHTS,
+    classes=len(CLASSES),
+    activation=ACTIVATION
+)
+
+preprocessing_fn = smp.encoders.get_preprocessing_fn(ENCODER, ENCODER_WEIGHTS)
+train_dataset = Dataset(
+    x_train_dir,
+    y_train_dir,
+    augmentation=get_training_augmentation(),
+    preprocessing=get_preprocessing(preprocessing_fn),
+    classes=CLASSES,
+)
+
+valid_dataset = Dataset(
+    x_valid_dir,
+    y_valid_dir,
+    augmentation=get_validation_augmentation(),
+    preprocessing=get_preprocessing(preprocessing_fn),
+    classes=CLASSES,
+)
+
+train_loader = DataLoader(train_dataset, batch_size=8, shuffle=True, num_workers=12)
+valid_loader = DataLoader(valid_dataset, batch_size=1, shuffle=False, num_workers=4)
+
+loss = smp.losses.DiceLoss()
+metrics = [
+    smp.utils.metrics.IoU(threshold=0.5),
+]
+optimizer = torch.optim.Adam([
+    dict(params=model.parameters(), lr=0.0001),
+])
+# create epoch runners
+# it is a simple loop of iterating over dataloader's samples
+train_epoch = smp.utils.train.TrainEpoch(
+    model,
+    loss=loss,
+    metrics=metrics,
+    optimizer=optimizer,
+    device=DEVICE,
+    verbose=True,
+)
+valid_epoch = smp.utils.train.ValidEpoch(
+    model,
+    loss=loss,
+    metrics=metrics,
+    device=DEVICE,
+    verbose=True,
+)
+# train model for 40 epochs
+max_score = 0
+for i in range(0, 40):
+    logging.info(f"\nEpoch: {i}")
+    train_logs = train_epoch.run(train_loader)
+    valid_logs = valid_epoch.run(valid_loader)
+    # do something (save model, change lr, etc.)
+    if max_score < valid_logs['iou_score']:
+        max_score = valid_logs['iou_score']
+        torch.save(model, './best_mode.pth')
+        logging.info('Model saved!')
+    if i == 25:
+        optimizer.param_groups[0]['lr'] = 1e-5
+        logging.info('Decrease decoder learning rate to 1e-5!')
 logging.info('Done!')
