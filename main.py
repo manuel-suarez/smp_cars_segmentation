5edec3b5519a74bba607c77382c4a64d3f8ae3f2
Arturo Suárez <manuel.suarez@cimat.mx>
2023-04-26 15:23:43 -0600

    import fix

--- main.py
+++ main.py
@@ -218,7 +218,7 @@ valid_dataset = Dataset(
 train_loader = DataLoader(train_dataset, batch_size=8, shuffle=True, num_workers=12)
 valid_loader = DataLoader(valid_dataset, batch_size=1, shuffle=False, num_workers=4)
 
-loss = smp.losses.DiceLoss()
+loss = smp.utils.losses.DiceLoss()
 metrics = [
     smp.utils.metrics.IoU(threshold=0.5),
 ]
